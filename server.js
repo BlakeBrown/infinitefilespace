fd94728673eb8b05c07e4a648fd3fbed6e9b8627
david <davidf1212@gmail.com>
2015-03-28 17:01:29 -0400

    cleanup

--- server.js
+++ server.js
@@ -41,15 +41,7 @@ function getFiles(callback) {
 
 		client.readdir('/', function (error, entries, folder_data, file_data) {
 			if (error) return showError(error);
-
-			// console.log('======= START OF ENTRIES =========');
-			// console.log(file_data);
-			// var files = file_data;
-			// for (var i = 0; i < files.length; i++) {
-			// 	console.log(client.thum bnailUrl(files[i].path));
-			// }
 			callback(file_data);
-			// console.log('======= END OF ENTRIES =========');
 		});
 
 		function showError (error) {
@@ -59,32 +51,33 @@ function getFiles(callback) {
 }
 
 app.get('/files', function (req, res) {
-
 	// Client files is the JSON object we send to the client, files is the JSON object from dropbox api
 	var clientFiles = [];
 
 	getFiles(function (files) {
-
-		for(var i = 0; i < files.length; i++) {
+		for (var i = 0, len = files.length; i < len; i++) {
 			clientFiles.push({
 				name: files[i].name,
 				hasThumbnail: false,
 				thumbnailUrl: null
 			});
 
-			if(files[i].hasThumbnail) {
+			if (files[i].hasThumbnail) {
 				clientFiles[i].hasThumbnail = true;
 				clientFiles[i].thumbnailUrl = client.thumbnailUrl(files[i].path);
 			}
 		}
 		// Send the JSON object to the client
 		res.json(clientFiles);
-
 	});
 });
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/upload', function (req, res) {
+	
 });
 
 http.listen(3000, function() {
