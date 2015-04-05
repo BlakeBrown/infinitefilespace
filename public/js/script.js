b972802ec6c66e9b0bb1cfd2c426a312146486be
Blake Brown <blakebrown129@gmail.com>
2015-04-05 16:01:49 -0400

    Small changes, refactor google drive logic into same file as dropbox

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,95 @@
-var client = new Dropbox.Client({ key: 'urwnqx6tzbbcjt2' });
+var dropbox_client = new Dropbox.Client({ key: 'urwnqx6tzbbcjt2' });
 var folderPath = '';
 
+var google_client_id = '766073445398-mj7vlvphri2a54pkmn1e9eapu3dkc6f3.apps.googleusercontent.com';
+var google_api_key = 'AIzaSyAOCKS4OLptIyuefC2OBdPEi9uJU840GAQ';
+
+//=============== GOOGLE DRIVE ================================ 
+
+// Use a button to handle authentication the first time.
+function handleClientLoad() {
+	gapi.client.setApiKey(google_api_key);
+	window.setTimeout(checkAuth,1);
+}
+
+function checkAuth() {
+	gapi.auth.authorize({client_id: google_client_id, scope: 'https://www.googleapis.com/auth/drive', immediate: true}, handleAuthResult);
+}
+
+function handleAuthResult(authResult) {
+	var authorizeButton = document.getElementById('add_account_btn');
+	if (authResult && !authResult.error) {
+		//authorizeButton.style.visibility = 'hidden';
+		makeApiCall();
+	} else {
+		authorizeButton.style.visibility = '';
+	}
+}
+
+// Load the API and make an API call.  Display the results on the screen.
+function makeApiCall() {
+	gapi.client.load('drive', 'v2', function() {
+
+		var request = gapi.client.drive.files.list ();
+
+		request.execute(function(resp) {          
+			for (i=0; i<resp.items.length; i++) {
+
+				var name = resp.items[i].title;
+				var url = resp.items[i].thumbnailLink;
+				var createdDate = Date.parse(resp.items[i].createdDate);
+				createdDate = timeSince(createdDate);
+				if(name.length > 40) {
+					name = name.substr(0, 40) + "...";
+				}
+				var icon = '<i class="fa fa-fw fa-file-' + getIcon(getType(name)) + '"></i>';
+				if (url) icon = '';
+
+				if(url) {   
+					card = '<div class="col-md-3 grid_item_container">\
+								<a href="' + url + '" download>\
+									<div class="grid_item grid_photo" style="background-image: url(' + url + ')"></div>\
+									<br>\
+									<label>' + name + '</label>\
+								</a>\
+								<br>\
+								<span style="font-size: 0.7em">' + createdDate + '</span>\
+							</div>';
+					$('#grid').append(card);
+				} else {
+					var card = '<div class="col-md-3 grid_item_container">\
+									<a href="' + url + '" download>\
+										<div class="grid_item">' + icon + '</div>\
+										<br>\
+										<label>' + name+ '</label>\
+									</a>\
+									<br>\
+									<span style="font-size: 0.7em">' + createdDate + '</span>\
+								</div>'; 
+					$('#grid').append(card);
+				}
+				//var dateCreated = resp.items[i].createdDate;
+				//var date = timeSince(dateCreated);
+			}
+			initGrid();
+		});        
+	});
+}
+
+// ============= END OF GOOGLE DRIVE =====================
+
 $(document).ready(function () {
-    if (client.isAuthenticated()) {
+    if (dropbox_client.isAuthenticated()) {
     	getUser();
-    	getFiles('');
+    	getFiles();
     } else {
-		client.authenticate(function (error, client) {
+		dropbox_client.authenticate(function (error, client) {
 	        if (error) {
 	        	console.log('Error: ' + error);
 	        	return;
 	        }
     		getUser();
-	        getFiles('');
+	        getFiles();
 	    });
     }
 });
@@ -22,17 +99,16 @@ function clearFiles() {
 }
 
 function getUser() {
-	client.getAccountInfo(function(error, info) {
+	dropbox_client.getAccountInfo(function(error, info) {
 		if (error) return showError(error);
-		console.log(info);
 	});
 }
 
-function getFiles(path) {
-	client.readdir('/' + path, function (error, entries, folder_data, file_data) {
+function getFiles() {
+	dropbox_client.readdir('/', function (error, entries, folder_data, file_data) {
 		var i = file_data.length;
 		file_data.forEach(function (file) {
-			client.makeUrl(file.path, {downloadHack: true}, function (error, file_data) {
+			dropbox_client.makeUrl(file.path, {downloadHack: true}, function (error, file_data) {
 				file.url = file_data.url;
 				var time = ('' + file.modifiedAt).substring(0, 24);
 				file.timeSincePosted = timeSince(Date.parse(time));
@@ -213,4 +289,11 @@ $("#list_files").on("click", function() {
 
 $("#sharing_link").on("click", function() {
 	alert("Please share Filespace with your friends! :)")
+});
+
+$("#add_account_btn").on("click", function(e) {
+	e.preventDefault();
+	$("#add_account_modal").show();
+	//Authorize with google drive
+	//gapi.auth.authorize({client_id: google_client_id, scope: scopes, immediate: false}, handleAuthResult);
 });
